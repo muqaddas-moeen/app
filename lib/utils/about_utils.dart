df9c08b6e7107010ce67a3a2fb953353e410a5ff
muqds-898 <muqaddas.bscs4103@iiu.edu.pk>
2023-12-04 23:35:24 +0500

    updated

---
+++ lib/utils/about_utils.dart
@@ -0,0 +1,7 @@
+class AboutUtils {
+  static const String aboutMeHeadline =
+      'I\'m Muqaddas Moeen, a Flutter developer.';
+
+  static const String aboutMeDetail =
+      ' Greetings!, I\'m a Fresh Graduate with Computer Science as Majors from International Islamic University, Islamabad. A passionate Flutter developer with a knack for crafting visually stunning and highly functional mobile applications. My mission is to transform ideas into seamless digital experiences that captivate users and elevate brands.  ';
+}
---
+++ lib/utils/project_utils.dart
@@ -0,0 +1,31 @@
+class ProjectUtils {
+  static const List<String> banners = [
+    "assets/projects/medkitB.png",
+    "assets/projects/hereiamB.png",
+    "assets/projects/covidB.png",
+  ];
+
+  static const List<String> icons = [
+    "assets/projects/medkit.png",
+    "assets/projects/hereiam.png",
+    "assets/projects/covid.png",
+  ];
+
+  static const List<String> titles = [
+    "MedKit",
+    "Here I Am",
+    "COVID-19",
+  ];
+
+  static const List<String> description = [
+    "A Phramacy app developed using Flutter powered with Firebase as database with Doctor and Patient panels.",
+    "Here I am is an Alert app that Sends alert SMS holding your location (Address and Google Maps) to your loved ones.",
+    "A live trakcer for COVID19 stats across the Globe and my Home country Pakistan. It uses APIs so the data is live.",
+  ];
+
+  static const List<String> links = [
+    "https://github.com/muqaddas-moeen/pharmacy",
+    "https://github.com/muqaddas-moeen/hereIam",
+    "https://github.com/muqaddas-moeen/covid-19",
+  ];
+}
